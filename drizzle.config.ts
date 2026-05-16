dd4d9f47073c8ca2a6126f33f639d1ee8b623853
Lucas Sachs <109156708+LucasSachs@users.noreply.github.com>
2026-05-16 20:47:30 -0300

    fix: Sets the drizzle to user Turso dialect

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   out: './src/database/drizzle',
   schema: './src/database/schema.ts',
-  dialect: 'sqlite',
+  dialect: 'turso',
   dbCredentials: { url: 'turso.db' },
 });
